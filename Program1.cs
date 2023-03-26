57c00399078621cee07fd9c0480389336bd0cd24
kou466 <32214586+kou466@users.noreply.github.com>
2023-03-26 18:53:50 +0900

    배열을 이용한 스택

---
+++ Program1.cs
@@ -0,0 +1,40 @@
+﻿/*using System;
+
+namespace Stack_report
+{
+    internal class Program1
+    {
+        static void Main(string[] args)
+        {
+            char[] parking = new char[5];
+            int top = 0;
+
+            parking[top] = 'A';
+            Console.WriteLine("{0} 자동차가 주차장에 들어감", parking[top]);
+            top++;
+
+            parking[top] = 'B';
+            Console.WriteLine("{0} 자동차가 주차장에 들어감", parking[top]);
+            top++;
+
+            parking[top] = 'C';
+            Console.WriteLine("{0} 자동차가 주차장에 들어감", parking[top]);
+            top++;
+
+            Console.WriteLine();
+
+            top--;
+            Console.WriteLine("{0} 자동차가 주차장을 빠져나감", parking[top]);
+            parking[top] = ' ';
+
+            top--;
+            Console.WriteLine("{0} 자동차가 주차장을 빠져나감", parking[top]);
+            parking[top] = ' ';
+
+            top--;
+            Console.WriteLine("{0} 자동차가 주차장을 빠져나감", parking[top]);
+            parking[top] = ' ';
+        }
+    }
+}
+*/
